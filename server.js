23acc42c3512c3558bd8e2aa1d45e1403a708a10
Parathps7 <77536966+Parathps7@users.noreply.github.com>
2024-07-15 18:16:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app =express();
 const port = 3000;
+const bodyparser = require("body-parser");
+const Razorpay = require("razorpay")
 
+app.use(bodyparser.json());
+var instance = new Razorpay({
+    key_id: '',
+    key_secret: ''
+})
 
 
 app.listen(port,()=>{console.log("App is running!")})
